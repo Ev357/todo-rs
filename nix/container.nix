3c917714ad885aa9ee9cff46849cb5ab233ed18f
Ev357 <evest.dev@gmail.com>
2026-09-12 12:54:33 +0200

    fix: nix container

--- nix/container.nix
+++ nix/container.nix
@@ -1,15 +1,17 @@
 {
-  boot.isContainer = true;
+  boot.isNspawnContainer = true;
 
   system.stateVersion = "26.05";
 
   services.todo-server = {
     enable = true;
+    address = "0.0.0.0";
     openFirewall = true;
   };
 
   services.todo-web = {
     enable = true;
+    address = "0.0.0.0";
     openFirewall = true;
   };
 }
